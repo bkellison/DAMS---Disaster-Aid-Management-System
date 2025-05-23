1498126542b609ac34964adc1828f66995a4065a
bkellison <bkellison@uiowa.edu>
2025-05-22 20:56:23 -0500

    backend fix

--- Backend/runserver.py
+++ Backend/runserver.py
@@ -4,5 +4,5 @@ from disaster_relief_app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))  # fallback for local dev
     app.run(host='0.0.0.0', port=port, debug=False)
